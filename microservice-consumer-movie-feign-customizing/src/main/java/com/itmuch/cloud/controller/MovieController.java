fe767b270dfadae2ba06f72f4038c4772ba71be8
Zhengtianqi <270490096@qq.com>
2018-08-23 10:42:10 +0800

    加入Feign的另一种方法

---
+++ microservice-consumer-movie-feign-customizing/src/main/java/com/itmuch/cloud/controller/MovieController.java
@@ -0,0 +1,22 @@
+package com.itmuch.cloud.controller;
+
+import com.itmuch.cloud.feign.UserFeignClient;
+import com.itmuch.cloud.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class MovieController {
+    @Autowired
+    private UserFeignClient userFeignClient;
+
+    @GetMapping("/movie/{id}") // 可能会超时
+    public User findById(@PathVariable Long id){
+        return userFeignClient.findById(id);
+    }
+
+    @GetMapping("/test")
+    public User testPost(User user){
+        return this.userFeignClient.postUser(user);
+    }
+}
