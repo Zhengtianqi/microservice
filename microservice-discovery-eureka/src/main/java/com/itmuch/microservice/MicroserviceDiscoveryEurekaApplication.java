f568a2cf97c619ed813d067fc9c64a40f95a1aab
Zhengtianqi <270490096@qq.com>
2018-08-29 10:37:18 +0800

    引入hystrix-dashboard时访问hystrix-stream失败

--- microservice-discovery-eureka/src/main/java/com/itmuch/microservice/MicroserviceDiscoveryEurekaApplication.java
+++ microservice-discovery-eureka/src/main/java/com/itmuch/microservice/MicroserviceDiscoveryEurekaApplication.java
@@ -1,4 +1,4 @@
-package com.itmuch.microservice.microservicediscoveryeureka;
+package com.itmuch.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
